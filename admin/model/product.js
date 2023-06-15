795bae7b3d8db1a1caf4e86ffd214e5a56b69159
Huydev0808 <phamngohoanghuy95@gmail.com>
2023-06-15 22:38:46 +0700

    Fix filter feature

--- admin/model/product.js
+++ admin/model/product.js
@@ -1,6 +1,6 @@
 export default class Product {
     constructor (
-        _id, _name, _price, _screen, _backCam, _frontCam, _img, _desc, _typePhone) 
+        _id, _name, _price, _screen, _backCam, _frontCam,_typePhone, _img, _desc) 
     {
         this.id = _id;
         this.name = _name;
@@ -8,8 +8,8 @@ export default class Product {
         this.screen = _screen;
         this.backCamera = _backCam;
         this.frontCamera = _frontCam;
+        this.type = _typePhone;
         this.img = _img;
         this.desc = _desc;
-        this.type = _typePhone;
     }
 };
